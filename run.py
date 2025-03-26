b91f8ace67f8f1d7b6795fe75221706552812dd6
burritosoftware <37681398+burritosoftware@users.noreply.github.com>
2025-03-25 21:15:47 -0700

    Add flask-login library to  app

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
-#from app import myapp_obj
-#myapp_obj.run()
-
+from flask_login import LoginManager
 import app
+
+login_manager = LoginManager()
+
+login_manager.init_app(app.myapp_obj)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return app.models.User.query.get({"id": user_id})
+
 app.myapp_obj.run(debug=True)
 
 
